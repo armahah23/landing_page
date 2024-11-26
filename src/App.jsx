e852e94b673754e00cdda537a8980eb86fc3d51d
armahah23 <armahemp.off@gmail.com>
2024-11-26 11:50:48 +0530

    add Feature and WorkFlow components; integrate into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Feature from "./components/Feature"
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
+import WorkFlow from "./components/WorkFlow"
 
 
 const App = () => {
@@ -8,6 +10,8 @@ const App = () => {
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
+        <Feature />
+        <WorkFlow />
       </div>
     </>
   )
